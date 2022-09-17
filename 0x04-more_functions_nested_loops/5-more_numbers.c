5b3bad2f3ffc06deab5df1bdbc82704acd79b5d5
Abimbolan <abimbolanw@gmail.com>
2022-09-16 21:41:23 -0700

    loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ for (i = 0; i < 10; i++)
 {
 for (ch = 0; ch < 15; ch++)
 {
-if (ch > +10)
+if (ch >= 10)
 _putchar((ch / 10) + 48);
-_putchar ((ch % 10) + 48);
+_putchar((ch % 10) + 48);
 }
 _putchar('\n');
 }
