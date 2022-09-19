2692a2d4ea21e75e9798f679cfb2b8a307e7124c
Abimbolan <abimbolanw@gmail.com>
2022-09-19 11:54:59 -0700

    10 pointers array

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @s: operand to be evaluated
+ */
+
+void print_rev(char *s)
+int main(void)
+{
+
+int len, i;
+
+for (len = 0; s[len] != '\0'; len++)
+{}
+
+len = len - 1;
+
+for (i = 0; len >= i; len--)
+{
+_putchar(s[len]);
+}
+
+_putchar('\n');
+}
