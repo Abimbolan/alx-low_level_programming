7c4163dbe237015e5c3c82c6f10dbcb79b1bfbf9
Abimbolan <abimbolanw@gmail.com>
2022-09-30 03:47:44 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - is a function that prints the multiplication of args
+ * @argc: is the arguments
+ * @argv: is the pointer to the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int x;
+int result = 1;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+for (x = 1; x < argc; x++)
+{
+result = result *atoi(argv[x]);
+}
+
+printf("%d\n", result);
+return (0);
+}
